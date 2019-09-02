31efa1dfbdd28ff033d7c924b1b12538f48f4c2b
Anderson <mfderson@gmail.com>
2019-09-02 20:17:05 -0300

    Adicionando model para appointments

---
+++ src/app/models/Appointments.js
@@ -0,0 +1,19 @@
+import Sequelize, { Model } from 'sequelize';
+
+class File extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+}
+
+export default File;
