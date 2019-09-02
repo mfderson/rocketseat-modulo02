66e789285b64222ab3b76f5cd13fd42098b2d26d
Anderson <mfderson@gmail.com>
2019-09-02 20:19:28 -0300

    Criando relacionamento appointment, user e provider

--- src/app/models/Appointments.js
+++ src/app/models/Appointments.js
@@ -14,6 +14,11 @@ class File extends Model {
 
     return this;
   }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
 }
 
 export default File;
