7e9839b9e7668c9e6d2b0231709dfafb868de031
Anderson <mfderson@gmail.com>
2019-09-17 18:07:23 -0300

    feature: paginando resultado de agendamentos

--- src/routes.js
+++ src/routes.js
@@ -25,7 +25,6 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
-
 routes.post('/appointments', AppointmentController.store);
 
 export default routes;
