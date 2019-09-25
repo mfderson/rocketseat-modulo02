6fd4115b94dc8f9be6480c271f3286e1b5e5d833
Anderson <mfderson@gmail.com>
2019-09-24 21:36:28 -0300

    feature: configuranto nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'b578d6b85f5aaa',
+    pass: '9ccb7ea0839801',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
