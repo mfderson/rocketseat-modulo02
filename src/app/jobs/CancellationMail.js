becf6ef7dd9c3c174159b2abfc50bbd0f5c07df2
Anderson <mfderson@gmail.com>
2019-09-25 21:37:15 -0300

    feature: configurando fila com redis

---
+++ src/app/jobs/CancellationMail.js
@@ -0,0 +1,36 @@
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Mail from '../../lib/Mail';
+
+class CancellationMail {
+  // Retorna a chave única para o nosso job
+  get key() {
+    return 'CancellationMail';
+  }
+
+  // Esta é a tarefa que será executada quando o job for executado
+  async handle({ data }) {
+    const { appointment } = data;
+
+    console.log('A fila executou');
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          parseISO(appointment.date),
+          "'dia' dd 'de' MMMM', às' H:mm'h'",
+          {
+            locale: pt,
+          }
+        ),
+      },
+    });
+  }
+}
+
+export default new CancellationMail();
---
+++ src/lib/Queue.js
@@ -0,0 +1,38 @@
+import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMail];
+
+class Queue {
+  constructor() {
+    // Criar uma fila para cada background job na minha aplicação
+    this.queues = {};
+
+    this.init();
+  }
+
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, { redis: redisConfig }),
+        handle,
+      };
+    });
+  }
+
+  // Adiciona um novo job dentro da fila
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.process(handle);
+    });
+  }
+}
+
+export default new Queue();
